90ae94c8f9f5e4bb30dd878459cab3e6831478bc
Jean Villegas <jeanvillegaslop@gmail.com>
2024-11-02 01:03:06 -0500

    primera prueba html

--- project/config/views.py
+++ project/config/views.py
@@ -1,1 +1,14 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+from django.shortcuts import render
+
+def prueba_template(request):
+    mi_html = open("./templates/template1.html")
+    mi_template = Template(mi_html.read())
+    mi_html.close()
+    mi_contexto = Context({"nombre": "el consexo"})
+    mi_documento = mi_template.render(mi_contexto)
+    return HttpResponse(mi_documento)
+
+def mis_notas(request):
+    return render(request, "notas.html")
