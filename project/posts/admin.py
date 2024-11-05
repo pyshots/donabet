40c83b838b46bfdd985629a7756636d31a73747b
Jean Villegas <jeanvillegaslop@gmail.com>
2024-11-04 20:47:44 -0500

    3 modelos nuevos, registrados

--- project/posts/admin.py
+++ project/posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Equipo)
+admin.site.register(models.Partido)
+admin.site.register(models.ResultadoPartido)
--- project/posts/models.py
+++ project/posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    liga = models.CharField(max_length=100)  # Copa Libertadores, Serie A...
+
+    def __str__(self):
+        return self.nombre
+
+class Partido(models.Model):
+    equipo_1 = models.ForeignKey(Equipo, related_name='equipo_1_partidos', on_delete=models.CASCADE)
+    equipo_2 = models.ForeignKey(Equipo, related_name='equipo_2_partidos', on_delete=models.CASCADE)
+    fecha = models.DateTimeField()
+    liga = models.CharField(max_length=100)  # Copa Libertadores, Serie A, etc.
+    prediccion = models.CharField(max_length=200, blank=True, null=True)  # Predicción del equipo favorito
+    explicacion = models.TextField(blank=True, null=True)  # Explicación generada por GPT
+
+    def __str__(self):
+        return f"{self.equipo_1} vs {self.equipo_2} - {self.fecha.strftime('%d/%m/%Y')}"
+
+class ResultadoPartido(models.Model):
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE, related_name='resultados')
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    goles_a_favor = models.IntegerField()  # Goles a favor
+    goles_en_contra = models.IntegerField()  # Goles en contra
+    resultado = models.CharField(max_length=10)  # 'Victoria', 'Derrota', 'Empate'
+
+    def __str__(self):
+        return f"{self.equipo.nombre}: {self.resultado} ({self.goles_a_favor} - {self.goles_en_contra})"
